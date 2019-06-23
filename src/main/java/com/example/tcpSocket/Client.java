e881809ca76aeda57269e7c7147489c89eb03c96
xianpeng.xia <951256916>
2019-06-23 17:15:49 +0800

    服务器返回响应，客户端接收服务器响应

--- src/main/java/com/example/tcpSocket/Client.java
+++ src/main/java/com/example/tcpSocket/Client.java
@@ -1,6 +1,9 @@
 package com.example.tcpSocket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -18,7 +21,18 @@ public class Client {
             PrintWriter printWriter = new PrintWriter(outputStream);
             printWriter.write("username:admin,password:1234");
             printWriter.flush();
-            socket.shutdownInput();
+            socket.shutdownOutput();
+            //获取服务器端响应信息
+            InputStream inputStream = socket.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String info = null;
+            while ((info = bufferedReader.readLine()) != null) {
+                System.out.println("我是客户端，服务器说：" + info);
+            }
+            bufferedReader.close();
+            inputStreamReader.close();
+            inputStream.close();
             printWriter.close();
             outputStream.close();
             socket.close();
--- src/main/java/com/example/tcpSocket/Server.java
+++ src/main/java/com/example/tcpSocket/Server.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -26,6 +28,14 @@ public class Server {
                 System.out.println("我是服务器，客户端说：" + info);
             }
             socket.shutdownInput();
+            // 服务器端响应
+            OutputStream outputStream = socket.getOutputStream();
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            printWriter.write("welcome");
+            printWriter.flush();
+
+            printWriter.close();
+            outputStream.close();
             br.close();
             inputStreamReader.close();
             inputStream.close();
